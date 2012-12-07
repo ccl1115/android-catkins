5d5aae7efe8be49f22917107b1d802c2feec47cf
lu.yu <bb.simon.yu@gmail.com>
2012-12-08 00:11:06 +0800

    init

---
+++ simon-android-demo-it/src/main/java/com/bbsimon/android/demo/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package com.bbsimon.android.demo.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.bbsimon.android.demo.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
