14c5fb6f3a8018d89c5d63804cf8f93e1a660961
GuruSimon <ccl1115@gmail.com>
2013-07-03 11:57:50 +0800

    working on indicator

--- catkins-demo/src/main/java/com/simon/catkins/demo/app/IndicatorDemoActivity.java
+++ catkins-demo/src/main/java/com/simon/catkins/demo/app/IndicatorDemoActivity.java
@@ -1,31 +1,65 @@
 package com.simon.catkins.demo.app;
 
 import android.os.Bundle;
-import android.app.FragmentManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.simon.catkins.app.BaseActivity;
 import com.simon.catkins.demo.R;
+import com.simon.catkins.views.pager.IndicatorView;
 
 /**
  * @author Simon
  */
 public class IndicatorDemoActivity extends BaseActivity {
+    private static final String TAG = "IndicatorDemoActivity";
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getTitleBar().setTitle(TAG);
+
         setContentView(R.layout.indicator_demo);
+
+        final IndicatorView indicatorView = (IndicatorView) findViewById(R.id.indicator);
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new PagerAdapter());
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                indicatorView.setPosition(position + positionOffset);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     class PagerAdapter extends FragmentPagerAdapter {
 
-        public PagerAdapter(FragmentManager fm) {
+        public PagerAdapter() {
             super(getSupportFragmentManager());
         }
 
         @Override
         public Fragment getItem(int position) {
-            return null;
+            return new Fragment() {
+                @Override
+                public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+                    View view = new View(container.getContext());
+                    view.setBackgroundColor(0xff329231);
+                    return view;
+                }
+            };
         }
 
         @Override
